d39ca77bf2890e289d27a590e30663a551b5b4aa
Ryan Waters <ryananthonywaters@gmail.com>
2014-03-22 12:05:18 -0700

    styling, playing with font awesome, beginning to refactor and add multiple resorts

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,9 @@
 
-var resultsDiv = document.getElementById('thumbnails');
+
 var apiKey = "AIzaSyB1cilOWhgIfYXGUZ21_UwUf_ON5jJpyGU";
 var request = new XMLHttpRequest();
+
+
 request.open('GET', "https://www.googleapis.com/youtube/v3/search?part=snippet&q=kirkwood+ski+resort&&order=date&key=" + apiKey);
 
   request.onreadystatechange = function() {
@@ -10,22 +12,13 @@ request.open('GET', "https://www.googleapis.com/youtube/v3/search?part=snippet&q
       var items = results.items;
       
       for (var i = 0; i < items.length; i++) {
-        var thumb = items[i].snippet.thumbnails.high.url;
         var videoId = items[i].id.videoId;
-        var img = document.createElement('img');
         var html_video = document.createElement('iframe');
+        var br = document.createElement('br');
         var videoLink = document.createElement('a');
-        var title = items[i].snippet.title;
-        img.setAttribute('src', thumb);
-        img.setAttribute('title', title);
-        img.setAttribute('alt', title);
-        videoLink.setAttribute('href', 'http://youtube.com/watch?v=' + videoId);
         html_video.setAttribute('src', 'http://youtube.com/embed/' + videoId + '?autoplay=1&controls=0&modestbranding=1');
-        
-      
-        // videoLink.appendChild(img);
         videos.appendChild(html_video);
-        // resultsDiv.appendChild(videoLink);
+        videos.appendChild(br);
       }
     }
   };
